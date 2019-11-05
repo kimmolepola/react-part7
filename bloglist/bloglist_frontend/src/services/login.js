323fad75dd5583bcf46c97b791b6dc2e2a69e6a8
Kimmo Lepola <kimmo.lepola@gmail.com>
2019-11-05 15:07:41 +0200

    front 09

--- bloglist/bloglist_frontend/src/services/login.js
+++ bloglist/bloglist_frontend/src/services/login.js
@@ -3,8 +3,14 @@ import axios from 'axios';
 const baseUrl = '/api/login';
 
 const login = async (credentials) => {
-  const response = await axios.post(baseUrl, credentials);
-  return response.data;
+  try{
+    const response = await axios.post(baseUrl, credentials);
+    return response.data;
+  } catch (exception) {
+    console.log('HERE IS MESSAGE', exception);
+
+  }
+
 };
 
 export default { login };
