502cb27cec951eaee56354d25d171b71cad157d7
Kimmo Lepola <kimmo.lepola@gmail.com>
2019-11-05 22:37:37 +0200

    frontend

--- bloglist/bloglist_frontend/src/services/blogs.js
+++ bloglist/bloglist_frontend/src/services/blogs.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = `https://tranquil-bayou-44537.herokuapp.com/api/blogs`;
+const baseUrl = `${process.env.REACT_APP_BACKEND}/api/blogs`;
 
 let config;
 
--- bloglist/bloglist_frontend/src/services/users.js
+++ bloglist/bloglist_frontend/src/services/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = `https://tranquil-bayou-44537.herokuapp.com/api/users`;
+const baseUrl = `${process.env.REACT_APP_BACKEND}/api/users`;
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
