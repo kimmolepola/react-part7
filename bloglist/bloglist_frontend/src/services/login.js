c48bd6f7f758bdd71c1b9ee25083ecd8c962714b
Kimmo Lepola <kimmo.lepola@gmail.com>
2019-11-05 21:51:09 +0200

    frontend

--- bloglist/bloglist_frontend/src/services/login.js
+++ bloglist/bloglist_frontend/src/services/login.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = `https://tranquil-bayou-44537.herokuapp.com/api/login`;
+const baseUrl = `${process.env.REACT_APP_BACKEND}/api/login`;
 
 const login = async (credentials) => {
     const response = await axios.post(baseUrl, credentials);
