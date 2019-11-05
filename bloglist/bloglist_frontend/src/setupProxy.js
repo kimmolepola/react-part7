5a45f2e0febf94d583df292ca1ad9d3ecd007ead
Kimmo Lepola <kimmo.lepola@gmail.com>
2019-11-05 19:00:42 +0200

    frontend

--- bloglist/bloglist_frontend/src/setupProxy.js
+++ bloglist/bloglist_frontend/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/api/**', { target: 'https://tranquil-bayou-44537.herokuapp.com:3000' }));
+    app.use(proxy('/api/**', { target: 'https://tranquil-bayou-44537.herokuapp.com' }));
 }
